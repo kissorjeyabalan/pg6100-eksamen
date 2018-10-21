fae96c36654bef5fcb97983ec4e492d9ff5f73e0
Christian Thorby <christianthorby@hotmail.com>
2018-10-21 11:12:14 +0200

    added dto

---
+++ ticket-service/src/main/kotlin/com/octopod/cinema/ticket/dto/TicketDto.kt
@@ -0,0 +1,23 @@
+package com.octopod.cinema.ticket.dto
+
+import io.swagger.annotations.ApiModelProperty
+import java.time.ZonedDateTime
+
+data class TicketDto (
+
+        @ApiModelProperty("The name of the person who bought the ticket")
+        var buyer: String? = null,
+
+        @ApiModelProperty("The name of the movie being shown")
+        var movieName: String? = null,
+
+        @ApiModelProperty("The time when the ticket was bought")
+        var timeOfPurchase: ZonedDateTime? = null,
+
+        @ApiModelProperty("The start time of the movie")
+        var movieStartTime: ZonedDateTime? = null,
+
+        @ApiModelProperty("The id of the ticket")
+        var id: String? = null
+
+)
--- ticket-service/src/main/kotlin/com/octopod/cinema/ticket/service/TicketService.kt
+++ ticket-service/src/main/kotlin/com/octopod/cinema/ticket/service/TicketService.kt
@@ -39,5 +39,4 @@ class TicketService {
         em.persist(ticket)
         return ticket.id
     }
-
 }
