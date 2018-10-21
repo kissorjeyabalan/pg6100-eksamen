8d2c7237e65190c3a60b79935958bc3957491480
Kissor Jeyabalan <git@kij.no>
2018-10-21 12:40:14 +0200

    Made constructors accessible from anywhere

--- common/src/main/kotlin/hateos/HalObject.kt
+++ common/src/main/kotlin/hateos/HalObject.kt
@@ -6,7 +6,7 @@ import io.swagger.annotations.ApiModelProperty
  * Taken from
  * https://github.com/arcuri82/testing_security_development_enterprise_systems/blob/ee5b64dc623abb1d344e0d2b8cde660358efbb89/advanced/rest/rest-dto/src/main/kotlin/org/tsdes/advanced/rest/dto/hal/HalObject.kt
  */
-open class HalObject {
+open class HalObject (
     @ApiModelProperty("HAL Links")
     var _links: MutableMap<String, HalLink> = mutableMapOf()
-}
+)
--- common/src/main/kotlin/hateos/HalPage.kt
+++ common/src/main/kotlin/hateos/HalPage.kt
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore
  * https://github.com/arcuri82/testing_security_development_enterprise_systems/blob/ee5b64dc623abb1d344e0d2b8cde660358efbb89/advanced/rest/rest-dto/src/main/kotlin/org/tsdes/advanced/rest/dto/hal/PageDto.kt
  */
 open class HalPage<T>(
-        var list: MutableList<T> = mutableListOf(),
+        var data: MutableList<T> = mutableListOf(),
         var pages: Int = 0,
         var count: Long = 0,
         next: HalLink? = null,
