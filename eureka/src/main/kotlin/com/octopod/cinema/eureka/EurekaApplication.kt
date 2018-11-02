47735f607d14f8cc15609865551631df18c9ae38
Kissor Jeyabalan <git@kij.no>
2018-11-02 15:42:24 +0100

    Implemented gatway

--- eureka/src/main/kotlin/com/octopod/cinema/eureka/EurekaApplication.kt
+++ eureka/src/main/kotlin/com/octopod/cinema/eureka/EurekaApplication.kt
@@ -3,10 +3,17 @@ package com.octopod.cinema.eureka
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.http.codec.ServerCodecConfigurer
 
 @EnableEurekaServer
 @SpringBootApplication
-class EurekaApplication
+class EurekaApplication {
+    @Bean
+    fun serverCodecConfigurer(): ServerCodecConfigurer {
+        return ServerCodecConfigurer.create()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<EurekaApplication>(*args)
