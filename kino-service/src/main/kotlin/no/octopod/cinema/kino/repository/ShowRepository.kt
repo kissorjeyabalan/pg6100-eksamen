bcfc6bbe44dd9f0019dfadf9018615a8837e0c00
Ritter <ritand16@student.westerdals.no>
2018-12-04 12:10:28 +0100

    changed names in the show repo

--- kino-service/src/main/kotlin/no/octopod/cinema/kino/repository/ShowRepository.kt
+++ kino-service/src/main/kotlin/no/octopod/cinema/kino/repository/ShowRepository.kt
@@ -1,12 +1,8 @@
 package no.octopod.cinema.kino.repository
 
 import no.octopod.cinema.kino.entity.ShowEntity
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import javax.persistence.EntityManager
-import javax.persistence.Id
-import javax.transaction.Transactional
 
 /**
  * Some functions may not be in use, but might be used in further development
