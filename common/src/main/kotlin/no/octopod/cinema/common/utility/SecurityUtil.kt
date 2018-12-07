cb55a866cfc900499479a3d8d02528be462b50aa
Kissor Jeyabalan <git@kij.no>
2018-12-07 16:48:40 +0100

    made userid optional

--- common/src/main/kotlin/no/octopod/cinema/common/utility/SecurityUtil.kt
+++ common/src/main/kotlin/no/octopod/cinema/common/utility/SecurityUtil.kt
@@ -4,12 +4,10 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 object SecurityUtil {
-    fun isAuthenticatedOrAdmin(authentication: Authentication, providedId: String?): Boolean {
+    fun isAuthenticatedOrAdmin(authentication: Authentication, providedId: String? = null): Boolean {
         if (!authentication.isAuthenticated) return false
         if (authentication.authorities.contains(SimpleGrantedAuthority("ROLE_ADMIN"))) return true
-        if (!providedId.isNullOrBlank()) {
-            if (authentication.name == providedId) return true
-        }
+        if (!providedId.isNullOrBlank() && authentication.name == providedId) return true
         return false
     }
 
