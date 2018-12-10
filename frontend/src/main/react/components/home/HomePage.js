348db142fb98d8ffb86bef12b8f9d9a0c6ee4fea
Christian Thorby <christianthorby@hotmail.com>
2018-12-10 16:50:26 +0100

    more styling

--- frontend/src/main/react/components/home/HomePage.js
+++ frontend/src/main/react/components/home/HomePage.js
@@ -15,36 +15,64 @@ class HomePage extends React.Component {
     }
 
     componentDidMount() {
-        axios.get(`${ApiBase.MOVIE_API}?featured=true`).then(res => {
+        /*axios.get(`${ApiBase.MOVIE_API}?featured=true`).then(res => {
             let movies = res.data
             this.setState({featuredMovies: movies})
+        })*/
+
+        let fakeReply = {
+            data: [
+                {
+                    id: 1,
+                    title: "he-man",
+                    description: "here is a loooooooooong description, dawdwada dwa d aw wda daw  dawd awdwad  d wd a w wd awd  dw"
+                },
+                {
+                    id: 2,
+                    title: "turtles",
+                    description: "here is a loooooooooong description, dawdwada dwa d aw wda daw  dawd awdwad  d wd a w wd awd  dw"
+                },
+                {
+                    id: 3,
+                    title: "barbie",
+                    description: "here is a loooooooooong description, dawdwada dwa d aw wda daw  dawd awdwad  d wd a w wd awd  dw"
+                },
+                {
+                    id: 4,
+                    title: "mega-man",
+                    description: "here is a loooooooooong description, dawdwada dwa d aw wda daw  dawd awdwad  d wd a w wd awd  dw"
+                },
+            ]
+        }
+
+        this.setState({
+            featuredMovies: fakeReply.data
         })
+
     }
-
-
 
     render() {
 
         let featuredMovieList = <div></div>
 
-        if(this.state.featuredMovies != null) {
-            featuredMovieList = <div className="featured-movie-list">
-                {this.state.featuredMovies.map(m => {
-                    <div key={"key_" + m.id} className="featured-movie">
-                        <p>m.title</p>
-                        <Link to={`/moviepage/${ this.props.id }`}>Movie Details</Link>
+        if(this.state.featuredMovies !== null){
+            featuredMovieList = <div className="movie-list">
+                {this.state.featuredMovies.map(m =>
+                    <div key={m.id} className="movie-list-card">
+                        <p className="movie-title" >Title: {m.title}</p>
+                        <p className="movie-desce" >Description: {m.description}</p>
                     </div>
-                })}
+                )}
             </div>
         }
 
         return(
             <div className="container">
-                <div>
+                <div className="header">
                     <h2>Movie-Service</h2>
                     <p>Here is a selection of this weeks featured movies</p>
-                    {featuredMovieList}
                 </div>
+                {featuredMovieList}
             </div>
         );    
     }
