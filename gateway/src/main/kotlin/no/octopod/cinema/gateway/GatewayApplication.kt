16b85d493c8a5eb6da0d3f9dc7b741ced2c271c1
Kissor Jeyabalan <git@kij.no>
2018-12-12 19:16:25 +0100

    fixed cors issue

--- gateway/src/main/kotlin/no/octopod/cinema/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/no/octopod/cinema/gateway/GatewayApplication.kt
@@ -3,10 +3,30 @@ package no.octopod.cinema.gateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Bean
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping
+import org.springframework.http.HttpMethod
+import java.util.*
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
-class GatewayApplication
+class GatewayApplication {
+    @Bean
+    fun corsConfiguration(routePredicateHandlerMapping: RoutePredicateHandlerMapping): CorsConfiguration {
+        val corsConfiguration = CorsConfiguration().applyPermitDefaultValues()
+        Arrays.asList(HttpMethod.OPTIONS, HttpMethod.PUT, HttpMethod.GET, HttpMethod.DELETE, HttpMethod.POST).forEach {
+            m -> corsConfiguration.addAllowedMethod(m) }
+        corsConfiguration.addAllowedOrigin("*")
+        routePredicateHandlerMapping.setCorsConfigurations(object : HashMap<String, CorsConfiguration>() {
+            init {
+                put("/**", corsConfiguration)
+            }
+        })
+        return corsConfiguration
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<GatewayApplication>(*args)
