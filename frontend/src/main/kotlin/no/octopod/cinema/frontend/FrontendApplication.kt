a57204d2d6a13b82b6c85b2b1c8fda099cd8748d
Kissor Jeyabalan <git@kij.no>
2018-12-13 20:29:47 +0100

    Added authentication to frontend

--- frontend/src/main/kotlin/no/octopod/cinema/frontend/FrontendApplication.kt
+++ frontend/src/main/kotlin/no/octopod/cinema/frontend/FrontendApplication.kt
@@ -2,8 +2,13 @@ package no.octopod.cinema.frontend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+
 
 
 
@@ -17,8 +22,8 @@ class FrontendApplication {
     fun index(): String {
         return "index"
     }
-
 }
+
 
 fun main(args: Array<String>) {
     runApplication<FrontendApplication>(*args)
